8de9eff5659a21af51625739ebba41a3be79beda
Sam Mulube <sam@umbrellium.co.uk>
2019-03-07 16:13:41 +0000

    Remove the placeholder single step

--- 01-citizen-credential-keygen.py
+++
@@ -1,7 +0,0 @@
-from zenroom import zenroom
-
-with open('dddc-pilot-contracts/src/01-CITIZEN-credential-keygen.zencode') as file:
-    script = file.read()
-
-result, errs = zenroom.execute(script.encode())
-print(result.decode())
